3589216ac1c85468ed4a3412d459d72b69a25361
jkyu <jkyu@stanford.edu>
2025-08-27 10:37:11 -0700

    make api initialization script also make the file folder

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,5 +1,6 @@
 from harmonization_framework.api.app import create_app
 from harmonization_framework.api.extensions import db
+import os
 
 app = create_app()
 
@@ -8,3 +9,10 @@ with app.app_context():
     print(app.config["SQLALCHEMY_DATABASE_URI"])
     print("Resolved path:", app.config["SQLALCHEMY_DATABASE_URI"].replace("sqlite:///", ""))
     print("Database Initialized")
+
+    upload_path = app.config["UPLOAD_FOLDER"]
+    if not os.path.exists(upload_path):
+        os.makedirs(upload_path)
+        print(f"Created uploads folder at: {upload_path}")
+    else:
+        print(f"Uploads folder already exists at: {upload_path}")
